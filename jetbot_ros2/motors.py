562f7fe37e4c17858f70f676a9d09f9e644a0e6b
Jake Keller <jakeller3630@gmail.com>
2020-09-25 17:04:37 -0400

    Initial Commit

---
+++ jetbot_ros2/motors.py
@@ -0,0 +1,89 @@
+# from math import sin, cos, radians
+import rclpy
+from rclpy.node import Node
+
+import qwiic_scmd
+from std_msgs.msg import String
+# from geometry_msgs.msg import Twist
+
+
+class MotorNode(Node):
+
+    def __init__(self):
+        super().__init__('motor_node')
+
+        self.motor_driver = qwiic_scmd.QwiicScmd()
+        self.motor_left_ID = 0
+        self.motor_right_ID = 1
+        self.all_stop()
+
+        self.string_sub = self.create_subscription(
+            String,
+            'jetbot_motor_string',
+            self.string_callback,
+            10)
+        
+# #TODO
+#        self.twist_sub = self.create_subscription(
+#            Twist,
+#            'jetbot_motor_twist',
+#            self.twist_callback,
+#            10)
+
+
+    # sets motor speed between [-1.0, 1.0]
+    def set_speed(self, motor_ID, value):
+        max_pwm = 115.0
+        #speed = int(min(max(abs(value * max_pwm), 0), max_pwm))
+        speed = int(value * max_pwm)
+    
+        if motor_ID == 0 or 1:
+            self.motor_driver.set_drive(motor_ID, 0, speed)
+        else:
+            self.get_logger().error('set_speed(%d, %f) -> invalid motor_ID=%d', motor_ID, value, motor_ID)
+            return
+    
+    # stops all motors
+    def all_stop(self):
+        self.motor_driver.set_drive(0,0,0)
+        self.motor_driver.set_drive(1,0,0)
+    
+        self.motor_driver.disable()
+
+    def string_callback(self, msg):
+        if msg.data == "left":
+            self.set_speed(self.motor_left_ID,  -1.0)
+            self.set_speed(self.motor_right_ID,  1.0)
+            self.motor_driver.enable()
+        elif msg.data == "right":
+            self.set_speed(self.motor_left_ID,   1.0)
+            self.set_speed(self.motor_right_ID, -1.0)
+            self.motor_driver.enable()
+        elif msg.data == "forward":
+            self.set_speed(self.motor_left_ID,   1.0)
+            self.set_speed(self.motor_right_ID,  1.0)
+            self.motor_driver.enable()
+        elif msg.data == "backward":
+            self.set_speed(self.motor_left_ID,  -1.0)
+            self.set_speed(self.motor_right_ID, -1.0)
+            self.motor_driver.enable()
+        elif msg.data == "stop":
+            self.all_stop()
+        else:
+            self.get_logger().error(f"Invalid cmd_str={msg.data}")
+
+#    def twist_callback(self, msg):
+#        pass
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    motor_node = MotorNode()
+
+    rclpy.spin(motor_node)
+    
+    motor_node.all_stop()
+    motor_node.destroy_node()
+    rclpy.shutdown()
+
+
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'jetbot_ros2'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Jake Keller',
+    maintainer_email='jakeller3630@gmail.com',
+    description='TODO: Package description',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'motor_node = jetbot_ros2.motors:main',
+        ],
+    },
+)
